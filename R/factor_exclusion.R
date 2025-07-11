2186e8e4cfc430b5985f9556d76b5c67ba650e28
André Achim <achim.andre@uqam.ca>
2025-07-11 16:53:13 -0400

    Avant de modifier exclude_fact pour lui transmettre des matrices plutôt que AS et des rangs de variables, pour pouvoir l'utiloser aussi pour l'annulation de contrastes excluant un facteur

--- R/factor_exclusion.R
+++ R/factor_exclusion.R
@@ -5,35 +5,55 @@ factor_exclusion <- function(AS,var=NULL){
   # A_excl et B_excl les contrastes de chacune des variables hors du facteur de A et B
   # lorsque la variable est partiellement annulée par A ou par B, l'autr servant de témoin de l'annulation
   if (is.null(var)) var <- AS$Z[1,1]
-  fct <- AS$VG[[1]]$Fct
-  factr <- which(fct[var,] != 0)[[1]]
-  AB <- which(fct[,factr] != 0)
+  # trouver la grappe qui contient var
+  gr <- AS$VG[[1]]$Gr
+  AB <- NULL
+  for (k in 1:length(gr)){
+    if (any(gr[[k]]==var)){
+      AB <- gr[[k]]
+      break 
+    }
+  }
+  if (is.null(AB))
+    stop("La variable désignée n'a pas d'annulation de son signal")
   AS$excl_AB <- AB
-  AS$A_excl <- exclude_fact(AS,AB[1],AB[2])
-  AS$B_excl <- exclude_fact(AS,AB[2],AB[1])
+#  browser()
+  for (k in 1:length(AB)){
+    out <- exclude_fact(AS,AB[k],AB[-k])
+    AS$excl_we[[k]] <- out$weigths
+    AS$excl_cr[[k]] <- out$crit 
+    AS$excl_co[[k]] <- out$contrasts 
+    AS$excl_pr[[k]] <- out$proj
+#    AS$excl[[k]] <- out
+  }
   return(AS)
 }
 
 exclude_fact <- function(AS,A,B){
-# ici
+# procède à l'annulation de toutes les variables (sauf celles dans A ou B)
+# avec la variable de rang A avec les variables de rangs B comme témoins
+# retourne une liste incluant le contraste et le critère 
   con <- matrix(0,AS$nv,AS$nv)
   wei <- numeric(AS$nv)  # cela laissera de 0 pour les variables pas dans AS$pertinent
   cri <- numeric(AS$nv)
-  pro <- numeric(AS$nv)
+  pro <- matrix(0,length(B),AS$nv)
   n=AS$N-1
   for (v in AS$pertinent){
+    if(!any(v==c(A,B))){
       p <- optimize(pair_cancel,c(-100,100),AS$GS[,c(A,v)],AS$GS[,B])
       wei[v] <- p$minimum
       out <- pair_cancel(wei[v],AS$GS[,c(A,v)],AS$GS[,B],as_list=TRUE)
       con[,v] <- out$contrast
       cri[v] <- out$crit * n
-      pro[v] <- out$proj
+      pro[,v] <- out$proj
+    }
   }
   return(list(weights=wei,crit=cri,contrasts=con,proj=pro))
 }
 
 corr_excl <- function(AS){
-  var <- setdiff(AS$pertinent,AS$excl_AB)
+  ote <- AS$excl_AB
+  var <- setdiff(AS$pertinent,ote)
   k <- length(var)
   corr_brut <- matrix(0,k,k)
   corr_mitig <- corr_brut
@@ -41,7 +61,10 @@ corr_excl <- function(AS){
     jj <- var[j]
     for (i in (j+1):k){
       ii <- var[i]
-      corr_brut[j,i] <- t(AS$A_excl$contrasts[,ii]) %*% AS$B_excl$contrasts[,jj]
+      if (any(ote==ii) | any(ote==jj))
+          corr_brut[j,i] <- NA_real_
+      else
+        corr_brut[j,i] <- t(AS$A_excl$contrasts[,ii]) %*% AS$B_excl$contrasts[,jj]
     }
   }
   AS$cor_exc <- list(var=var,corr_brut=corr_brut,corr_mitig=corr_mitig)
